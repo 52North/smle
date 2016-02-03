1066f2b23e923a526108035cca7ffb7ddec502dd
Christian Autermann <c.autermann@52north.org>
2016-02-03 18:56:57 +0100

    added angular2-webpack-starter project

---
+++ src/app/app.ts
@@ -0,0 +1,43 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {Component, ViewEncapsulation} from 'angular2/core';
+import {RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
+import {FORM_PROVIDERS} from 'angular2/common';
+
+import {RouterActive} from './directives/router-active';
+import {Home} from './home/home';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  providers: [ ...FORM_PROVIDERS ],
+  directives: [ ...ROUTER_DIRECTIVES, RouterActive ],
+  encapsulation: ViewEncapsulation.None,
+  pipes: [],
+  styles: [ require('./app.scss') ],
+  template: require('./app.html')
+})
+@RouteConfig([
+  { path: '/', component: Home, name: 'Index' },
+  { path: '/home', component: Home, name: 'Home' },
+  // Async load a component using Webpack's require with es6-promise-loader
+  { path: '/about', loader: () => require('./about/about')('About'), name: 'About' },
+  { path: '/**', redirectTo: ['Index'] }
+])
+export class App {
+  constructor() {
+  }
+}
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ * or via chat on Gitter at https://gitter.im/AngularClass/angular2-webpack-starter
+ */
---
+++ src/app/home/home.ts
@@ -0,0 +1,46 @@
+import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES} from 'angular2/common';
+
+import {Title} from './services/title';
+import {XLarge} from './directives/x-large';
+
+import {PhysicalSystem} from '../model/sensorML';
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'home'
+  selector: 'home',  // <home></home>
+  // We need to tell Angular's Dependency Injection which providers are in our app.
+  providers: [
+    Title
+  ],
+  // We need to tell Angular's compiler which directives are in our template.
+  // Doing so will allow Angular to attach our behavior to an element
+  directives: [
+    ...FORM_DIRECTIVES,
+    XLarge
+  ],
+  // We need to tell Angular's compiler which custom pipes are in our template.
+  pipes: [ ],
+  // Our list of styles in our component. We may add more to compose many styles together
+  styles: [ require('./home.scss') ],
+  // Every Angular template is first compiled by the browser before Angular runs it's compiler
+  template: require('./home.html')
+})
+export class Home {
+  // Set our default values
+  data = { value: '' };
+  
+  sensorML = new PhysicalSystem();
+  // TypeScript public modifiers
+  constructor(public title: Title) {
+
+  }
+
+  ngOnInit() {
+    console.log('hello `Home` component');
+    // this.title.getData().subscribe(data => this.data = data);
+  }
+
+}
