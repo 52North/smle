85e39a4972c47c2fd47d7d5d3e8175ef1b3bde92
Christian Autermann <c.autermann@52north.org>
2016-02-17 18:25:59 +0100

    splitted encoder

---
+++ src/app/services/xml/prefixes.ts
@@ -0,0 +1,8 @@
+
+export const GCO = 'gco';
+export const GMD = 'gmd';
+export const GML = 'gml';
+export const SML = 'sml';
+export const XSI = 'xsi';
+export const SWE = 'swe';
+export const XLINK = 'xlink';
