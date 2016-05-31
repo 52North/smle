c6043e1f2ec45528e972cad2abc4fce9e677e102
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-05-31 22:08:19 +0500

    Set up horizontal scroll style

--- src/app/editor/components/CardHeaderComponent.ts
+++ src/app/editor/components/CardHeaderComponent.ts
@@ -1,21 +1,23 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
-  selector: 'card-header',
-  template: require('./CardHeaderComponent.html')
+    selector: 'card-header',
+    template: require('./CardHeaderComponent.html'),
+    styles: [require('./styles/card-header-component.scss')]
 })
 export class CardHeaderComponent {
-  @Input()
-  public title: string;
-  @Output()
-  public reset: EventEmitter<any> = new EventEmitter();
-  @Output()
-  public close: EventEmitter<any> = new EventEmitter();
+    @Input()
+    public title:string;
+    @Output()
+    public reset:EventEmitter<any> = new EventEmitter();
+    @Output()
+    public close:EventEmitter<any> = new EventEmitter();
 
-  public onReset(): void {
-    this.reset.emit(null);
-  }
-  public onClose(): void {
-    this.close.emit(null);
-  }  
+    public onReset():void {
+        this.reset.emit(null);
+    }
+
+    public onClose():void {
+        this.close.emit(null);
+    }
 }
