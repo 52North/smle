ab07c449690fe07d4181de8224f22b675a758dda
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-06-04 16:54:24 +0500

    Make KeywordList component work

--- src/app/editor/components/swe/KeywordListComponent.ts
+++ src/app/editor/components/swe/KeywordListComponent.ts
@@ -2,11 +2,15 @@ import {Component, ComponentResolver, ViewContainerRef} from '@angular/core';
 import {AbstractMetadataListComponent} from './AbstractMetadataListComponent';
 import {KeywordList} from '../../../model/sml/KeywordList';
 import {AbstractComponent} from '../AbstractComponent';
+import {CardHeaderComponent} from '../CardHeaderComponent';
+import {StringsComponent} from '../StringsComponent';
 
 @Component({
     selector: 'keyword-list',
     template: require('./KeywordListComponent.html'),
-    directives: [AbstractMetadataListComponent]
+    host: {'[class.has-child]': 'hasChild'},
+    styles: [require('../styles/editor-component.scss')],
+    directives: [AbstractMetadataListComponent, CardHeaderComponent, StringsComponent]
 })
 export class KeywordListComponent extends AbstractComponent<KeywordList> {
     constructor(componentResolver: ComponentResolver, viewContainerRef: ViewContainerRef) {
