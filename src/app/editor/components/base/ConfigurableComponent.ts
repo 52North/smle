112b298ec17c3e88b3bc55df30ac8207d0d8ffa2
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-06-18 12:56:19 +0500

    Add show all property to Configurable component

--- src/app/editor/components/base/ConfigurableComponent.ts
+++ src/app/editor/components/base/ConfigurableComponent.ts
@@ -1,5 +1,28 @@
 import {Configuration} from '../../../services/config/Configuration';
+import {TrueConfiguration} from '../../../services/config/TrueConfiguration';
 
 export abstract class ConfigurableComponent {
-    public config: Configuration;
+    public config: Configuration = new TrueConfiguration();
+    private _isShowAll: boolean = false;
+    private bufferConfig: Configuration;
+
+    public get isShowAll(): boolean {
+        return this._isShowAll;
+    }
+
+    public set isShowAll(value: boolean) {
+        if (value === this._isShowAll) {
+            return;
+        }
+
+        if (value && !this._isShowAll) {
+            this.bufferConfig = this.config;
+            this.config = new TrueConfiguration();
+            this._isShowAll = true;
+        } else {
+            this.config = this.bufferConfig;
+            this.bufferConfig = null;
+            this._isShowAll = false;
+        }
+    }
 }
