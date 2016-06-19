25b8cb0c091ccc00a9d75c429a5da4ac0e169da2
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-06-19 15:38:04 +0500

    Fix all tslint errors

--- src/app/services/InMemoryDescriptionRepository.ts
+++ src/app/services/InMemoryDescriptionRepository.ts
@@ -1,76 +1,75 @@
-import { AbstractProcess } from '../model/sml';
-import { CodeType } from '../model/gml';
-import { Injectable } from '@angular/core';
-import { DescriptionRepository } from './DescriptionRepository';
-import { SampleDataLoader } from '../services/SampleDataLoader';
+import {AbstractProcess} from '../model/sml';
+import {CodeType} from '../model/gml';
+import {Injectable} from '@angular/core';
+import {DescriptionRepository} from './DescriptionRepository';
+import {SampleDataLoader} from '../services/SampleDataLoader';
 
 @Injectable()
 export class InMemoryDescriptionRepository extends DescriptionRepository {
-  private _descriptions: { [key: string]: AbstractProcess } = {};
-  /*
-  constructor(processes?: AbstractProcess[]) {
-    super();
-    if (processes) {
-      processes.reduce((o, d) => {
-        o[this._getId(d)] = d;
-        return o;
-      }, this._descriptions);
+    private _descriptions: { [key: string]: AbstractProcess } = {};
+    /*
+     constructor(processes?: AbstractProcess[]) {
+     super();
+     if (processes) {
+     processes.reduce((o, d) => {
+     o[this._getId(d)] = d;
+     return o;
+     }, this._descriptions);
+     }
+     }
+     */
+
+    private _samples: string[] = [
+        '/examples/physicalComponentInstance.xml',
+        '/examples/physicalComponentType.xml',
+        '/examples/physicalSystemInstance.xml',
+        '/examples/physicalSystemType.xml',
+        '/examples/lisaInstance.xml'
+    ];
+
+    constructor(private dataloader: SampleDataLoader) {
+        super();
     }
-  }
-  */
 
-  constructor(
-    private dataloader: SampleDataLoader
-  ) {
-    super();
-  }
-
-  private _samples: string[] = [
-    '/examples/physicalComponentInstance.xml',
-    '/examples/physicalComponentType.xml',
-    '/examples/physicalSystemInstance.xml',
-    '/examples/physicalSystemType.xml',
-    '/examples/lisaInstance.xml'
-  ];
-
-  getDescriptions(): Promise<Array<string>> {
-    let list = this._samples;
-    list = list.concat(Object.keys(this._descriptions));
-    return Promise.resolve(list);
-  }
-
-  getDescription(id: string): Promise<AbstractProcess> {
-    if (id.indexOf('/examples') > -1) {
-      return this.dataloader.loadSample(id);
+    getDescriptions(): Promise<Array<string>> {
+        let list = this._samples;
+        list = list.concat(Object.keys(this._descriptions));
+        return Promise.resolve(list);
     }
-    if (!this._descriptions[id]) {
-      return Promise.reject<AbstractProcess>(new Error('does not exist'));
-    };
-    return Promise.resolve(this._descriptions[id]);
-  }
 
-  saveDescription(description: AbstractProcess): Promise<void> {
-    let id = this._getId(description);
-    if (this._descriptions[id]) {
-      return Promise.reject(new Error('already saved'));
+    getDescription(id: string): Promise<AbstractProcess> {
+        if (id.indexOf('/examples') > -1) {
+            return this.dataloader.loadSample(id);
+        }
+        if (!this._descriptions[id]) {
+            return Promise.reject<AbstractProcess>(new Error('does not exist'));
+        }
+        ;
+        return Promise.resolve(this._descriptions[id]);
     }
-    this._descriptions[id] = description;
-    return Promise.resolve();
-  }
 
-  updateDescription(description: AbstractProcess): Promise<void> {
-    let id = this._getId(description);
-    if (!this._descriptions[id]) {
-      return Promise.reject(new Error('not yet saved'));
+    saveDescription(description: AbstractProcess): Promise<void> {
+        let id = this._getId(description);
+        if (this._descriptions[id]) {
+            return Promise.reject(new Error('already saved'));
+        }
+        this._descriptions[id] = description;
+        return Promise.resolve();
     }
-    this._descriptions[id] = description;
-    return Promise.resolve();
-  }
 
-  private _getId(description: AbstractProcess): string {
-    if (description.identifier == null || description.identifier.value == null) {
-      description.identifier = new CodeType(Date.now().toString());
+    updateDescription(description: AbstractProcess): Promise<void> {
+        let id = this._getId(description);
+        if (!this._descriptions[id]) {
+            return Promise.reject(new Error('not yet saved'));
+        }
+        this._descriptions[id] = description;
+        return Promise.resolve();
     }
-    return description.identifier.value;
-  }
+
+    private _getId(description: AbstractProcess): string {
+        if (description.identifier == null || description.identifier.value == null) {
+            description.identifier = new CodeType(Date.now().toString());
+        }
+        return description.identifier.value;
+    }
 }
