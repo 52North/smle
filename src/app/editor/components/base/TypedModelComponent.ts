81ff9dfb21cc88ce660ce59b08c2f855bcc8c876
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-07-02 18:51:41 +0500

    Refactor TimeListComponent

--- src/app/editor/components/base/TypedModelComponent.ts
+++ src/app/editor/components/base/TypedModelComponent.ts
@@ -1,5 +1,30 @@
-import {Input} from '@angular/core';
+import {Input, Output, EventEmitter} from '@angular/core';
 import {Configuration} from '../../../services/config/Configuration';
+import {Type} from '@angular/core/src/facade/lang';
+
+export class ChildMetadata {
+    private _componentType: Type;
+    private _model: any;
+    private _config: Configuration;
+
+    public get componentType(): Type {
+        return this._componentType;
+    }
+
+    public get model(): any {
+        return this._model;
+    }
+
+    public get config(): Configuration {
+        return this._config;
+    }
+
+    constructor(componentType: Type, model: any, config: Configuration) {
+        this._componentType = componentType;
+        this._model = model;
+        this._config = config;
+    }
+}
 
 export abstract class TypedModelComponent<T> {
     @Input()
@@ -8,6 +33,9 @@ export abstract class TypedModelComponent<T> {
     public config: Configuration;
     @Input()
     public isShowAll: boolean = false;
+
+    @Output()
+    public openAsChild: EventEmitter<ChildMetadata> = new EventEmitter<ChildMetadata>();
 
     protected abstract createModel(): T;
 
--- src/app/editor/components/basic/TimeListComponent.ts
+++ src/app/editor/components/basic/TimeListComponent.ts
@@ -1,71 +1,51 @@
-import {Component, ComponentResolver, ViewContainerRef, Input, Output, EventEmitter} from '@angular/core';
-import {EditorComponent} from '../base/EditorComponent';
+import {Component} from '@angular/core';
 import {AbstractTime} from '../../../model/gml/AbstractTime';
 import {TimePeriod} from '../../../model/gml/TimePeriod';
 import {TimeInstantComponent} from '../gml/TimeInstantComponent';
 import {TimePeriodComponent} from '../gml/TimePeriodComponent';
 import {TimeInstant} from '../../../model/gml/TimeInstant';
-import {Configuration} from '../../../services/config/Configuration';
+import {TypedModelComponent, ChildMetadata} from '../base/TypedModelComponent';
 
 @Component({
-  selector: 'time-list',
-  template: require('./TimeListComponent.html'),
-  directives: [TimeInstantComponent, TimePeriodComponent]
+    selector: 'time-list',
+    template: require('./TimeListComponent.html'),
+    directives: [TimeInstantComponent, TimePeriodComponent]
 })
-export class TimeListComponent extends EditorComponent<AbstractTime> {
-  @Input()
-  public list: AbstractTime[];
-  @Input()
-  public config: Configuration;
-  @Output()
-  public openAsChild: EventEmitter<any> = new EventEmitter<any>();
+export class TimeListComponent extends TypedModelComponent<Array<AbstractTime>> {
+    protected createModel() {
+        return undefined;
+    }
 
-  constructor(componentResolver: ComponentResolver, viewContainerRef: ViewContainerRef) {
-    super(componentResolver, viewContainerRef);
-  }
+    public removeItem(i: number): void {
+        this.model.splice(i, 1);
+    }
 
-  protected createModel() {
-    return undefined;
-  }
+    public addTime(): void {
+        let instant = new TimeInstant();
+        instant.time = new Date();
+        this.model.push(instant);
+    }
 
-  public removeItem(i: number): void {
-    this.list.splice(i, 1);
-  }
+    public addPeriod(): void {
+        var period = new TimePeriod();
+        period.begin = new Date();
+        period.end = new Date();
+        this.model.push(period);
+    }
 
-  public addTime(): void {
-    let instant = new TimeInstant();
-    instant.time = new Date();
-    this.list.push(instant);
-  }
+    public openTimeInstant(item: TimeInstant): void {
+        this.openAsChild.emit(new ChildMetadata(TimeInstantComponent, item, this.config.getConfigFor('timeInstant')));
+    }
 
-  public addPeriod(): void {
-    var period = new TimePeriod();
-    period.begin = new Date();
-    period.end = new Date();
-    this.list.push(period);
-  }
+    public openTimePeriod(item: TimePeriod): void {
+        this.openAsChild.emit(new ChildMetadata(TimePeriodComponent, item, this.config.getConfigFor('timePeriod')));
+    }
 
-  public openTimeInstant(model: TimeInstant): void {
-    this.openAsChild.emit({
-      component: TimeInstantComponent,
-      model: model,
-      config: this.config.getConfigFor('timeInstant')
-    });
-  }
+    private isPeriod(time: TimePeriod | any): boolean {
+        return typeof time.begin !== 'undefined' && typeof time.end !== 'undefined';
+    }
 
-  public openTimePeriod(model: TimePeriod): void {
-    this.openAsChild.emit({
-      component: TimePeriodComponent,
-      model: model,
-      config: this.config.getConfigFor('timePeriod')
-    });
-  }
-
-  private isPeriod(time: TimePeriod | any): boolean {
-    return typeof time.begin !== 'undefined' && typeof time.end !== 'undefined';
-  }
-
-  private onChange(time: AbstractTime, index: number) {
-    this.list[index] = time;
-  }
+    private onChange(time: AbstractTime, index: number) {
+        this.model[index] = time;
+    }
 }
