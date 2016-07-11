a5c3b2f2b6266cb951ac4d124e29888e0bdb85f1
root <j.schulte@52north.org>
2016-07-11 15:12:52 +0200

    add identifier to editor

--- src/app/editor/components/gml/CodeTypeComponent.ts
+++ src/app/editor/components/gml/CodeTypeComponent.ts
@@ -3,11 +3,11 @@ import {CodeType} from '../../../model/gml/CodeType';
 import {TypedModelComponent} from '../base/TypedModelComponent';
 
 @Component({
-    selector: 'gml-code-type',
-    template: require('./CodeTypeComponent.html')
+  selector: 'gml-code-type',
+  template: require('./CodeTypeComponent.html')
 })
 export class CodeTypeComponent extends TypedModelComponent<CodeType> {
-    protected createModel(): CodeType {
-        return undefined;
-    }
+  protected createModel(): CodeType {
+    return undefined;
+  }
 }
