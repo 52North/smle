533e4bc1278f440a137d9e9d670f0ae4b945086f
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-07-12 22:08:18 +0500

    Simplify array visualization

--- src/app/editor/components/basic/ObjectTreeComponent.ts
+++ src/app/editor/components/basic/ObjectTreeComponent.ts
@@ -17,29 +17,62 @@ export class ObjectTreeComponent implements OnChanges {
         this.nodes = this.getNodes(this.model);
     }
 
-    private getNodes(object: Object): Array<INode> {
-        var nodes: Array<INode> = [];
+    private getNodes(object: any): Array<INode> {
+        var nodes: Array<INode>;
 
-        if (object === null || typeof object === 'undefined') {
+        if (object === null || typeof object === 'undefined' || typeof object === 'function') {
             return null;
         }
+
+        if (object instanceof Array) {
+            nodes = this.getNodesForArray(object);
+        } else if (typeof object === 'object' && !(object instanceof Date)) {
+            nodes = this.getNodesForObject(object);
+        } else {
+            nodes = [{name: object.toString(), children: null}];
+        }
+
+        return nodes;
+    }
+
+    private getNodesForObject(object: Object): Array<INode> {
+        var nodes: Array<INode> = [];
+
         for (let propertyName in object) {
-            if (Object.prototype.hasOwnProperty(propertyName) || Array.prototype.hasOwnProperty(propertyName)) {
+            if (Object.prototype.hasOwnProperty(propertyName)) {
                 continue;
             }
 
             let newNode: INode = {name: propertyName, children: null};
             let nodeValue: any = object[propertyName];
 
-            if (typeof nodeValue === 'object') {
-                newNode.children = this.getNodes(nodeValue);
-            } else if (typeof nodeValue !== 'undefined' && nodeValue !== null && typeof nodeValue !== 'function') {
-                newNode.children = [{name: nodeValue.toString(), children: null}];
-            }
-
+            newNode.children = this.getNodes(nodeValue);
             nodes.push(newNode);
         }
 
+        return nodes;
+    }
+
+    private getNodesForArray(array: Array): Array<INode> {
+        var nodes = <Array>array.map((elem: any) => {
+            var node: INode = {name: null, children: null};
+
+            if (typeof elem === 'object' && !(elem instanceof Date) || elem instanceof Array) {
+                if (typeof elem.label === 'string' && elem.label.length) {
+                    node.name = elem.label;
+                } else if (typeof elem.name === 'string' && elem.name.length) {
+                    node.name = elem.name;
+                } else {
+                    node.name = elem.constructor.name;
+                }
+
+                node.children = this.getNodes(elem);
+            } else {
+                node.name = elem.toString();
+            }
+
+            return node;
+        });
 
         return nodes;
     }
