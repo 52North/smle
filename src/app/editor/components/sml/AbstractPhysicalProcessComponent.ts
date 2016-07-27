231f7b8d536e51c8334cf624db173503982c0398
Jan Schulte <j.schulte@52north.org>
2016-07-27 10:19:05 +0200

    add attached to element

--- src/app/editor/components/sml/AbstractPhysicalProcessComponent.ts
+++ src/app/editor/components/sml/AbstractPhysicalProcessComponent.ts
@@ -1,16 +1,17 @@
-import {Component} from '@angular/core';
-import {TypedModelComponent} from '../base/TypedModelComponent';
-import {AbstractProcessComponent} from './AbstractProcessComponent';
-import {AbstractPhysicalProcess} from '../../../model/sml/AbstractPhysicalProcess';
-import {PositionListComponent} from '../basic/PositionListComponent';
+import { Component } from '@angular/core';
+import { TypedModelComponent } from '../base/TypedModelComponent';
+import { AbstractProcessComponent } from './AbstractProcessComponent';
+import { AbstractPhysicalProcess } from '../../../model/sml/AbstractPhysicalProcess';
+import { PositionListComponent } from '../basic/PositionListComponent';
 
 @Component({
-    selector: 'sml-abstract-physical-process',
-    template: require('./AbstractPhysicalProcessComponent.html'),
-    directives: [AbstractProcessComponent, PositionListComponent]
+  selector: 'sml-abstract-physical-process',
+  template: require('./AbstractPhysicalProcessComponent.html'),
+  styles: [require('../styles/editor-component.scss')],
+  directives: [AbstractProcessComponent, PositionListComponent]
 })
 export class AbstractPhysicalProcessComponent extends TypedModelComponent<AbstractPhysicalProcess> {
-    protected createModel(): AbstractPhysicalProcess {
-        return undefined;
-    }
+  protected createModel(): AbstractPhysicalProcess {
+    return undefined;
+  }
 }
