2256083aeb73d6de14fc3d7779076c302ddb384e
Jan Schulte <j.schulte@52north.org>
2016-08-10 14:20:39 +0200

    add authorization component to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,26 @@
 /*
  * Angular 2 decorators and services
  */
-import {Component, ViewEncapsulation, ViewContainerRef} from '@angular/core';
-import {Modal, BS_MODAL_PROVIDERS} from 'angular2-modal/plugins/bootstrap';
+import { Component, ViewEncapsulation, ViewContainerRef } from '@angular/core';
+import { Modal, BS_MODAL_PROVIDERS } from 'angular2-modal/plugins/bootstrap';
+import { AuthComponent } from './sos/components/auth.component';
 
 /*
  * App Component
  * Top Level Component
  */
 @Component({
-    selector: 'app',
-    providers: [],
-    directives: [],
-    pipes: [],
-    encapsulation: ViewEncapsulation.None,
-    viewProviders: [...BS_MODAL_PROVIDERS],
-    styles: [require('./app.scss')],
-    template: require('./app.html')
+  selector: 'app',
+  providers: [],
+  directives: [AuthComponent],
+  pipes: [],
+  encapsulation: ViewEncapsulation.None,
+  viewProviders: [...BS_MODAL_PROVIDERS],
+  styles: [require('./app.scss')],
+  template: require('./app.html')
 })
 export class Application {
-    constructor(public modal: Modal, viewContainer: ViewContainerRef) {
-        modal.defaultViewContainer = viewContainer;
-    }
+  constructor(public modal: Modal, viewContainer: ViewContainerRef) {
+    modal.defaultViewContainer = viewContainer;
+  }
 }
