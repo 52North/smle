332559fea0cbb59f3e638f283034ac43837ea03c
Jan Schulte <j.schulte@52north.org>
2016-08-10 23:49:28 +0200

    use loggedInUser to hide delete menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@
 import { Component, ViewEncapsulation, ViewContainerRef } from '@angular/core';
 import { Modal, BS_MODAL_PROVIDERS } from 'angular2-modal/plugins/bootstrap';
 import { AuthComponent } from './sos/components/auth.component';
+import { AuthService } from './sos/components/auth.service';
 
 /*
  * App Component
@@ -20,7 +21,11 @@ import { AuthComponent } from './sos/components/auth.component';
   template: require('./app.html')
 })
 export class Application {
-  constructor(public modal: Modal, viewContainer: ViewContainerRef) {
+  constructor(
+    public modal: Modal,
+    viewContainer: ViewContainerRef,
+    private authService: AuthService
+  ) {
     modal.defaultViewContainer = viewContainer;
   }
 }
