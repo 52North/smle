8bfa85ba8de0c36a6a51711ff63bf1fe4490d67f
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2016-08-13 13:52:06 +0500

    Add TimePositionComponent

--- src/app/editor/components/swe/TimePositionComponent.ts
+++ src/app/editor/components/swe/TimePositionComponent.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnChanges, SimpleChanges} from '@angular/core';
 import {TypedModelComponent} from '../base/TypedModelComponent';
 import {TimePosition} from '../../../model/swe/TimePosition';
 import {DatePickerComponent} from '../gml/DatePickerComponent';
@@ -6,12 +6,35 @@ import {DatePickerComponent} from '../gml/DatePickerComponent';
 @Component({
     selector: 'swe-time-position',
     template: require('./TimePositionComponent.html'),
+    styles: ['label {display: inline-block; margin-bottom: 0;} label:not(:only-child) {margin-bottom: 8px;}'],
     directives: [DatePickerComponent]
 })
-export class TimePositionComponent extends TypedModelComponent<TimePosition> {
+export class TimePositionComponent extends TypedModelComponent<TimePosition> implements OnChanges {
+    private dateValue: Date = new Date();
 
+    private isDate() {
+        return this.model instanceof Date;
+    }
+
+    private dateValueChange(value: Date) {
+        if (this.isDate()) {
+            this.modelChange.emit(value);
+        }
+    }
+
+
+    private onChecked(value) {
+        this.modelChange.emit(value);
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        var modelChange = changes['model'];
+        if (modelChange && this.isDate()) {
+            this.dateValue = <Date>this.model;
+        }
+    }
 
     protected createModel(): TimePosition {
-        return new TimePosition();
+        return new Date();
     }
 }
