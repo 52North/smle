6c067715e9fc4c22d9937fdae2e739492ebebeef
Jan Schulte <j.schulte@52north.org>
2016-08-31 16:24:49 +0200

    create new base configuration for smle

---
+++ src/app/services/ConfigurationService.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class ConfigurationService {
+
+  public config: Configuration;
+
+  constructor(private http: Http) {
+    this.config = new Configuration();
+    this.config.sosUrl = 'http://localhost:8081/52n-sos-webapp/service';
+    this.config.proxyUrl = 'http://localhost:8081/52n-sos-webapp/service';
+    //this.fetchConfiguration();
+  }
+
+  public fetchConfiguration(): Promise<void> {
+    return this.http.get('./config.json').toPromise().then((response: Response) => {
+      var data = response.json();
+      this.config = data;
+    }).catch(() => {
+      console.error('Configuration for smle wasnt found.');
+    });
+  }
+}
+
+export class Configuration {
+  public sosUrl: string;
+  public proxyUrl: string;
+}
