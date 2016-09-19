51ff35f01aae1b7939d9c28655c6e28c8cf0e843
Jan Schulte <j.schulte@52north.org>
2016-09-19 10:42:32 +0200

    select description componet works with authorization parameter

--- src/app/sos/components/selectDescription.component.ts
+++ src/app/sos/components/selectDescription.component.ts
@@ -2,63 +2,66 @@ import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange
 import { SosService } from '../sos.service';
 
 @Component({
-  selector: 'description-id-selection',
-  template: require('./selectDescription.template.html')
+    selector: 'description-id-selection',
+    template: require('./selectDescription.template.html')
 })
 export class DescriptionSelection implements OnInit, DoCheck {
 
-  private descriptionIds: Array<string>;
-  private loadingDescriptionIds: boolean;
-  private oldIgnoreIdsCount: number = 0;
+    private descriptionIds: Array<string>;
+    private loadingDescriptionIds: boolean;
+    private oldIgnoreIdsCount: number = 0;
 
-  @Input()
-  public ignoreIds: Array<string>;
+    @Input()
+    public ignoreIds: Array<string>;
 
-  @Output()
-  public onSelectedDescription: EventEmitter<SelectedDescription> = new EventEmitter<SelectedDescription>();
+    @Input()
+    public authorized: boolean = false;
 
-  constructor(
-    private sosService: SosService
-  ) { }
+    @Output()
+    public onSelectedDescription: EventEmitter<SelectedDescription> = new EventEmitter<SelectedDescription>();
 
-  public ngOnInit() {
-    this.loadDescIds();
-  }
+    constructor(
+        private sosService: SosService
+    ) { }
 
-  public ngDoCheck() {
-    if (this.ignoreIds && this.ignoreIds.length !== this.oldIgnoreIdsCount) {
-      this.oldIgnoreIdsCount = this.ignoreIds.length;
-      this.loadDescIds();
+    public ngOnInit() {
+        this.loadDescIds();
     }
-  }
 
-  public loadDescIds() {
-    this.loadingDescriptionIds = true;
-    this.sosService.fetchDescriptionIDs().subscribe(res => {
-      this.loadingDescriptionIds = false;
-      this.descriptionIds = res;
-      if (this.ignoreIds && this.ignoreIds.length > 0) {
-        this.ignoreIds.forEach(ignoreId => {
-          var idx = this.descriptionIds.indexOf(ignoreId);
-          if (idx >= -1) {
-            this.descriptionIds.splice(idx, 1);
-          }
+    public ngDoCheck() {
+        if (this.ignoreIds && this.ignoreIds.length !== this.oldIgnoreIdsCount) {
+            this.oldIgnoreIdsCount = this.ignoreIds.length;
+            this.loadDescIds();
+        }
+    }
+
+    public loadDescIds() {
+        this.loadingDescriptionIds = true;
+        this.sosService.fetchDescriptionIDs(this.authorized).subscribe(res => {
+            this.loadingDescriptionIds = false;
+            this.descriptionIds = res;
+            if (this.ignoreIds && this.ignoreIds.length > 0) {
+                this.ignoreIds.forEach(ignoreId => {
+                    var idx = this.descriptionIds.indexOf(ignoreId);
+                    if (idx >= -1) {
+                        this.descriptionIds.splice(idx, 1);
+                    }
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  public onSelectDescriptionID(descId: string) {
-    this.sosService.fetchDescription(descId)
-      .subscribe(res => {
-        this.onSelectedDescription.emit(new SelectedDescription(descId, res));
-      });
-  }
+    public onSelectDescriptionID(descId: string) {
+        this.sosService.fetchDescription(descId)
+            .subscribe(res => {
+                this.onSelectedDescription.emit(new SelectedDescription(descId, res));
+            });
+    }
 }
 
 export class SelectedDescription {
-  constructor(
-    public id: string,
-    public description: string
-  ) { }
+    constructor(
+        public id: string,
+        public description: string
+    ) { }
 }
--- src/app/sos/sos.service.ts
+++ src/app/sos/sos.service.ts
@@ -12,184 +12,185 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class SosService {
 
-  private sosUrl: string = 'http://192.168.52.145:8081/52n-sos-webapp/service';
-  private proxyUrl: string = 'http://192.168.52.145:8082/api/proxy';
+    private sosUrl: string;
+    private proxyUrl: string;
 
-  constructor(
-    private http: Http,
-    private configurationService: ConfigurationService
-  ) {
-    this.sosUrl = this.configurationService.config.sosUrl;
-    this.proxyUrl = this.configurationService.config.proxyUrl;
-  }
-
-  public fetchDescriptionIDs(sosUrl?: string): Observable<Array<string>> {
-    let body = JSON.stringify({
-      'request': 'GetCapabilities',
-      'service': 'SOS',
-      'sections': [
-        'OperationsMetadata'
-      ]
-    });
-    return this.http.post(this.useProxyUrl(sosUrl), body, {
-      headers: this.createJsonHeader(),
-      withCredentials: true
-    })
-      .map(this.extractDescriptionIDs)
-      .catch(this.handleError);
-  }
-
-  public fetchDescription(descId: string, sosUrl?: string): Observable<string> {
-    let body = JSON.stringify({
-      'request': 'DescribeSensor',
-      'service': 'SOS',
-      'version': '2.0.0',
-      'procedure': descId,
-      'procedureDescriptionFormat': 'http://www.opengis.net/sensorml/2.0'
-    });
-    return this.http.post(this.useSosUrl(sosUrl), body, {
-      headers: this.createJsonHeader(),
-      withCredentials: true
-    })
-      .map((res) => {
-        let json = res.json();
-        return json.procedureDescription.description || json.procedureDescription;
-      });
-  }
-
-  public deleteDescription(descId: string, sosUrl?: string): Observable<boolean> {
-    let body = JSON.stringify({
-      'request': 'DeleteSensor',
-      'service': 'SOS',
-      'version': '2.0.0',
-      'procedure': descId
-    });
-    return this.http.post(this.useProxyUrl(sosUrl), body, {
-      headers: this.createJsonHeader(),
-      withCredentials: true
-    })
-      .map((res) => {
-        let json = res.json();
-        return json.deletedProcedure === descId ? true : false;
-      });
-  }
-
-  public hasSosDescription(descID: string, sosUrl?: string): Observable<boolean> {
-    return new Observable<boolean>((observer: Observer<boolean>) => {
-      this.fetchDescriptionIDs(this.useSosUrl(sosUrl)).subscribe((res) => {
-        if (res) {
-          res.forEach((entry) => {
-            if (entry === descID) {
-              observer.next(true);
-              observer.complete();
-            }
-          });
-        }
-        observer.next(false);
-        observer.complete();
-      }, (error) => {
-        observer.error(error);
-        observer.complete();
-      });
-    });
-  }
-
-  public addDescription(description: AbstractProcess, sosUrl?: string): Observable<boolean> {
-    let body = JSON.stringify({
-      'request': 'InsertSensor',
-      'service': 'SOS',
-      'version': '2.0.0',
-      'procedureId': description.identifier.value,
-      'procedureDescriptionFormat': 'http://www.opengis.net/sensorml/2.0',
-      'procedureDescription': new SensorMLXmlService().serialize(description),
-      // featureOfInterest auswählbar machen
-      'featureOfInterestType': 'http://www.opengis.net/def/samplingFeatureType/OGC-OM/2.0/SF_SamplingPoint',
-      // observationType auswählbar machen ???
-      'observationType': [
-        'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement'
-      ],
-      // observableProperty auswählbar machen ???
-      'observableProperty': [
-        'http://www.52north.org/test/observableProperty/9_1']
-    });
-    return this.http.post(this.useProxyUrl(sosUrl), body, {
-      headers: this.createJsonHeader(),
-      withCredentials: true
-    })
-      .map(this.handleAddDescription)
-      .catch(this.handleAddDescriptionError);
-  }
-
-  public updateDescription(descID: string, description: AbstractProcess, sosUrl?: string): Observable<boolean> {
-    let body = JSON.stringify({
-      'request': 'UpdateSensorDescription',
-      'service': 'SOS',
-      'version': '2.0.0',
-      'procedure': descID,
-      'procedureDescriptionFormat': 'http://www.opengis.net/sensorml/2.0',
-      'procedureDescription': new SensorMLXmlService().serialize(description)
-    });
-    return this.http.post(this.useProxyUrl(sosUrl), body, {
-      headers: this.createJsonHeader(),
-      withCredentials: true
-    })
-      .map(this.handleAddDescription)
-      .catch(this.handleAddDescriptionError);
-  }
-
-  public createDescribeSensorUrl(identifier: string): string {
-    let url = this.sosUrl;
-    url += '?service=SOS';
-    url += '&version=2.0.0';
-    url += '&request=DescribeSensor';
-    url += '&procedureDescriptionFormat=http://www.opengis.net/sensorml/2.0';
-    url += '&procedure=' + identifier;
-    return url;
-  }
-
-  public getIdentifierOfDescribeSensorUrl(describeSensorUrl: string): string {
-    let index = describeSensorUrl.indexOf('procedure=');
-    return describeSensorUrl.substr(index + 10);
-  }
-
-  private createJsonHeader(): Headers {
-    return new Headers({
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    });
-  }
-
-  private extractDescriptionIDs(res: Response): Array<string> {
-    let json = res.json();
-    return json.operationMetadata.operations.DescribeSensor.parameters.procedure.allowedValues;
-  }
-
-  private handleAddDescription(res: Response): boolean {
-    return true;
-  }
-
-  private handleAddDescriptionError(error: Response) {
-    let json = error.json();
-    if (json.exceptions && json.exceptions.length >= -1) {
-      let errors: Array<string> = [];
-      (json.exceptions as Array<any>).forEach(entry => {
-        errors.push(entry.locator || entry.text);
-      });
-      return Observable.throw(errors);
+    constructor(
+        private http: Http,
+        private configurationService: ConfigurationService
+    ) {
+        this.sosUrl = this.configurationService.config.sosUrl;
+        this.proxyUrl = this.configurationService.config.proxyUrl;
     }
-    return Observable.throw([this.handleError(error)]);
-  }
 
-  private handleError(res: Response) {
-    if (res.status === 0) return Observable.throw('Could not reach the service!');
-  }
+    public fetchDescriptionIDs(authorized?: boolean, sosUrl?: string): Observable<Array<string>> {
+        let body = JSON.stringify({
+            'request': 'GetCapabilities',
+            'service': 'SOS',
+            'sections': [
+                'OperationsMetadata'
+            ]
+        });
+        let url = authorized ? this.useProxyUrl(sosUrl) : this.useSosUrl(sosUrl);
+        return this.http.post(url, body, {
+            headers: this.createJsonHeader(),
+            withCredentials: true
+        })
+            .map(this.extractDescriptionIDs)
+            .catch(this.handleError);
+    }
 
-  private useSosUrl(sosUrl: string) {
-    return sosUrl || this.sosUrl;
-  }
+    public fetchDescription(descId: string, sosUrl?: string): Observable<string> {
+        let body = JSON.stringify({
+            'request': 'DescribeSensor',
+            'service': 'SOS',
+            'version': '2.0.0',
+            'procedure': descId,
+            'procedureDescriptionFormat': 'http://www.opengis.net/sensorml/2.0'
+        });
+        return this.http.post(this.useSosUrl(sosUrl), body, {
+            headers: this.createJsonHeader(),
+            withCredentials: true
+        })
+            .map((res) => {
+                let json = res.json();
+                return json.procedureDescription.description || json.procedureDescription;
+            });
+    }
 
-  private useProxyUrl(proxyUrl: string) {
-    return proxyUrl || this.proxyUrl;
-  }
+    public deleteDescription(descId: string, sosUrl?: string): Observable<boolean> {
+        let body = JSON.stringify({
+            'request': 'DeleteSensor',
+            'service': 'SOS',
+            'version': '2.0.0',
+            'procedure': descId
+        });
+        return this.http.post(this.useProxyUrl(sosUrl), body, {
+            headers: this.createJsonHeader(),
+            withCredentials: true
+        })
+            .map((res) => {
+                let json = res.json();
+                return json.deletedProcedure === descId ? true : false;
+            });
+    }
+
+    public hasSosDescription(descID: string, sosUrl?: string): Observable<boolean> {
+        return new Observable<boolean>((observer: Observer<boolean>) => {
+            this.fetchDescriptionIDs(false, this.useSosUrl(sosUrl)).subscribe((res) => {
+                if (res) {
+                    res.forEach((entry) => {
+                        if (entry === descID) {
+                            observer.next(true);
+                            observer.complete();
+                        }
+                    });
+                }
+                observer.next(false);
+                observer.complete();
+            }, (error) => {
+                observer.error(error);
+                observer.complete();
+            });
+        });
+    }
+
+    public addDescription(description: AbstractProcess, sosUrl?: string): Observable<boolean> {
+        let body = JSON.stringify({
+            'request': 'InsertSensor',
+            'service': 'SOS',
+            'version': '2.0.0',
+            'procedureId': description.identifier.value,
+            'procedureDescriptionFormat': 'http://www.opengis.net/sensorml/2.0',
+            'procedureDescription': new SensorMLXmlService().serialize(description),
+            // featureOfInterest auswählbar machen
+            'featureOfInterestType': 'http://www.opengis.net/def/samplingFeatureType/OGC-OM/2.0/SF_SamplingPoint',
+            // observationType auswählbar machen ???
+            'observationType': [
+                'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement'
+            ],
+            // observableProperty auswählbar machen ???
+            'observableProperty': [
+                'http://www.52north.org/test/observableProperty/9_1']
+        });
+        return this.http.post(this.useProxyUrl(sosUrl), body, {
+            headers: this.createJsonHeader(),
+            withCredentials: true
+        })
+            .map(this.handleAddDescription)
+            .catch(this.handleAddDescriptionError);
+    }
+
+    public updateDescription(descID: string, description: AbstractProcess, sosUrl?: string): Observable<boolean> {
+        let body = JSON.stringify({
+            'request': 'UpdateSensorDescription',
+            'service': 'SOS',
+            'version': '2.0.0',
+            'procedure': descID,
+            'procedureDescriptionFormat': 'http://www.opengis.net/sensorml/2.0',
+            'procedureDescription': new SensorMLXmlService().serialize(description)
+        });
+        return this.http.post(this.useProxyUrl(sosUrl), body, {
+            headers: this.createJsonHeader(),
+            withCredentials: true
+        })
+            .map(this.handleAddDescription)
+            .catch(this.handleAddDescriptionError);
+    }
+
+    public createDescribeSensorUrl(identifier: string): string {
+        let url = this.sosUrl;
+        url += '?service=SOS';
+        url += '&version=2.0.0';
+        url += '&request=DescribeSensor';
+        url += '&procedureDescriptionFormat=http://www.opengis.net/sensorml/2.0';
+        url += '&procedure=' + identifier;
+        return url;
+    }
+
+    public getIdentifierOfDescribeSensorUrl(describeSensorUrl: string): string {
+        let index = describeSensorUrl.indexOf('procedure=');
+        return describeSensorUrl.substr(index + 10);
+    }
+
+    private createJsonHeader(): Headers {
+        return new Headers({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        });
+    }
+
+    private extractDescriptionIDs(res: Response): Array<string> {
+        let json = res.json();
+        return json.operationMetadata.operations.DescribeSensor.parameters.procedure.allowedValues;
+    }
+
+    private handleAddDescription(res: Response): boolean {
+        return true;
+    }
+
+    private handleAddDescriptionError(error: Response) {
+        let json = error.json();
+        if (json.exceptions && json.exceptions.length >= -1) {
+            let errors: Array<string> = [];
+            (json.exceptions as Array<any>).forEach(entry => {
+                errors.push(entry.locator || entry.text);
+            });
+            return Observable.throw(errors);
+        }
+        return Observable.throw([this.handleError(error)]);
+    }
+
+    private handleError(res: Response) {
+        if (res.status === 0) return Observable.throw('Could not reach the service!');
+    }
+
+    private useSosUrl(sosUrl: string) {
+        return sosUrl || this.sosUrl;
+    }
+
+    private useProxyUrl(proxyUrl: string) {
+        return proxyUrl || this.proxyUrl;
+    }
 
 }
