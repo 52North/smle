1a732d20d8fbab55ace6f0e675d7b28cdf829a91
Jan Schulte <j.schulte@52north.org>
2018-04-24 13:53:11 +0200

    configuration adjustments

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { configPromise } from './app/configuration';
+import { ingestionConfigPromise } from './app/ingestion/ingestion.config.service';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-Promise.all([configPromise]).then((config: any) => {
+Promise.all([configPromise, ingestionConfigPromise]).then((config: any) => {
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.log(err));
 });
